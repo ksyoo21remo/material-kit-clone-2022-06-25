e41063dca8253bf8f55505b775bebd38cfd88f56
YooKiseong <ksyoo21remo@gmail.com>
2022-06-21 14:02:26 +0900

    install react-router-dom, @mui packages
    react-router-dom is for navigation between pages
    @mui is a material-ui component library

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Home() {
+  return (
+    <Box>
+      <Typography variant={"body1"}>홈페이지</Typography>
+    </Box>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+function Login() {
+  return (
+    <Box>
+      <Typography variant={"body1"}>로그인 페이지</Typography>
+    </Box>
+  );
+}
+
+export default Login;
