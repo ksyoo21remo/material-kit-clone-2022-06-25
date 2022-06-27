32578c5e4f587fa73744d1815f1dcce9fc190b72
ksyoo <ksyoo21remo@gmail.com>
2022-06-27 23:30:16 +0900

    page Dashboard/Customers/:id/Edit

--- frontend/src/pages/Dashboard/Customers/Id/Edit/index.tsx
+++ frontend/src/pages/Dashboard/Customers/Id/Edit/index.tsx
@@ -1,14 +1,105 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
-import { useParams } from "react-router-dom";
+import {
+  Avatar,
+  Box,
+  Chip,
+  Container,
+  Typography,
+} from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import React, { useEffect, useState } from "react";
+import { Link as RouterLink, useParams } from "react-router-dom";
+import { Customer } from "../../types";
+import { customerApi } from "../../../../../__fake-api__/customerApi";
+import { getInitials } from "../../../../../utils/getInitials";
+import CustomerEditForm from "./CustomerEditForm";
 
 export default function Edit() {
+  // eslint-disable-next-line
   const { id: customerId } = useParams();
+  // eslint-disable-next-line
+  const [customer, setCustomer] = useState<Customer | null>(null);
+
+  useEffect(() => {
+    async function setCustomerData() {
+      const data = await customerApi.getCustomer();
+      setCustomer(data);
+    }
+    setCustomerData();
+  }, []);
 
   return (
     <>
-      <Box>
-        <Typography>{`Customer ${customerId}'s Edit Page`}</Typography>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: "background.default",
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth="md">
+          <Box sx={{ mb: 4 }}>
+            <RouterLink to={`/dashboard/customers`}>
+              <Box
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                }}
+              >
+                <ArrowBackIcon fontSize="small" sx={{ mr: 1 }} />
+                <Typography variant="subtitle2">Customers</Typography>
+              </Box>
+            </RouterLink>
+          </Box>
+          {customer && (
+            <>
+              <Box
+                sx={{
+                  alignItems: "center",
+                  display: "flex",
+                  overflow: "hidden",
+                }}
+              >
+                <Avatar
+                  src={customer.avatar}
+                  sx={{
+                    height: 64,
+                    mr: 2,
+                    width: 64,
+                  }}
+                >
+                  {getInitials(customer.name)}
+                </Avatar>
+                <Box>
+                  <Typography noWrap variant="h4">
+                    {customer.email}
+                  </Typography>
+                  <Box
+                    sx={{
+                      alignItems: "center",
+                      display: "flex",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      whiteSpace: "nowrap",
+                    }}
+                  >
+                    <Typography variant="subtitle2">
+                      user_id:
+                    </Typography>
+                    <Chip
+                      label={customer.id}
+                      size="small"
+                      sx={{ ml: 1 }}
+                    />
+                  </Box>
+                </Box>
+              </Box>
+              <Box mt={3}>
+                <CustomerEditForm customer={customer} />
+              </Box>
+            </>
+          )}
+        </Container>
       </Box>
     </>
   );
